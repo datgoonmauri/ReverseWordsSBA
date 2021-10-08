192acbec359e3dea67b306a023e1f43f6e681f83
datgoonmauri <mauriciomj11@gmail.com>
2021-10-08 09:42:41 -0700

    completed commit

--- src/org/perscholas/Main.java
+++ src/org/perscholas/Main.java
@@ -1,8 +1,30 @@
 package org.perscholas;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+
+    public static void main(String[] args)
+    {
+        char[] message = { 'c', 'a', 'k', 'e', ' ',
+                'p', 'o', 'u', 'n', 'd', ' ',
+                's', 't', 'e', 'a', 'l' };
+        String reverseWords = new String(message).toString();
+        System.out.println(reverseWords);
+        String[] split = reverseWords.split(" ");
+
+        String result;
+        if (split.length > 4) {
+            result = split[0] + " " + split[2] + " " + split[2];
+        } else {
+            result = split[2] + " " + split[1] + " " + split[0];
+        }
+
+        System.out.println(result);
+
+
     }
-}
+    }
---
+++ src/org/perscholas/README.md
@@ -0,0 +1,11 @@
+Your team is scrambling to decipher a recent message, worried it's a plot to break into a major European National Cake Vault. The message has been mostly deciphered, but all the words are backward! Your colleagues have handed off the last step to you.
+
+Write a method reverseWords() that takes a message as an array of characters and reverses the order of the words in place.
+
+For example:
+
+char[] message = { 'c', 'a', 'k', 'e', ' ', 'p', 'o', 'u', 'n', 'd', ' ', 's', 't', 'e', 'a', 'l' };
+reverseWords(message);
+System.out.println(message); // prints: "steal pound cake"
+
+When writing your method, assume the message contains only letters and spaces, and all words are separated by one space.
